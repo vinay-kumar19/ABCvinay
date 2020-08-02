11cafa14e26771b53e9dd926a5ff20d987c08a58
vinay kumar thallapelli <vinaykumar.thallapelli@infor.com>
2020-08-02 09:25:45 +0530

    Revert "git reset"
    This reverts commit b3d0f5ec7295ef5c599271411a0b0808521619ae.

--- GMOpackage/src/com/git/practice/Ayu.java
+++ GMOpackage/src/com/git/practice/Ayu.java
@@ -5,8 +5,6 @@ public class Ayu {
 	public static void main(String[] args) {
 		// TODO Auto-generated method s
 		System.out.println("Ayu123");
-		System.out.println("Ayu321");
-		System.out.println("Ayu234");
 	}
 
 }
