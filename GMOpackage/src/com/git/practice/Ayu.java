e9d6df8a9177c861a3ee05d4046e83b9ca665e62
vinay-kumar19 <vinay.thallapelli@gmail.com>
2021-05-04 19:51:18 +0530

    Update Ayu.java
    commit3

--- GMOpackage/src/com/git/practice/Ayu.java
+++ GMOpackage/src/com/git/practice/Ayu.java
@@ -1,14 +1,9 @@
 package com.git.practice;
 
 public class Ayu {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method s
 		System.out.println("Ayu123");
-	
-
-
-
+		System.out.println("Ayu124");
 	}
-
 }
