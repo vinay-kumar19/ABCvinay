831a1aa6a449fa89ce93cd6e60dcf3d98025aaac
vinay <vinay.thallapelli@gmail.com>
2021-05-04 19:49:53 +0530

    commit2

--- GMOpackage/src/com/git/practice/Ayu.java
+++ GMOpackage/src/com/git/practice/Ayu.java
@@ -5,16 +5,7 @@ public class Ayu {
 	public static void main(String[] args) {
 		// TODO Auto-generated method s
 		System.out.println("Ayu123");
-		System.out.println("Ayu1234");
-		System.out.println("Ayu12345");
-		System.out.println("Ayu123456");
-		System.out.println("Commit");
-		System.out.println("Commit7688");
-		System.out.println("Ayu7688");
-		System.out.println("Ayu8688");
-
-		System.out.println("Commit8688");
-		System.out.println("Commit8689");
+	
 
 
 
